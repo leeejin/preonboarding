56c6abd1b97f0e344d29b0178999d0d94f6e77be
leeejin <asd72621@naver.com>
2024-09-08 12:54:39 +0900

    Add : Jest 추가

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,10 @@
+// Imports
+import { render } from "@testing-library/react";
+// To Test
+import App from "../App";
+
+// Tests
+test("Renders main page correctly", async () => {
+  // Setup
+  render(<App />);
+});
