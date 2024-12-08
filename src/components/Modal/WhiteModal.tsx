52db860fd27b6f76bfe1beb28c4b9538c5d3b9b3
leeejin <asd72621@naver.com>
2024-12-08 23:17:58 +0900

    Fix : cva를 input, whitemodal에 적용

--- src/components/Modal/WhiteModal.tsx
+++ src/components/Modal/WhiteModal.tsx
@@ -1,9 +1,24 @@
-import clsx from "clsx";
+import { cva } from "class-variance-authority";
 import { useEffect, useState } from "react";
 import { TToast } from "../../contexts/toast.context";
 interface WhiteModalProps {
   toast: TToast; // toast prop의 타입 정의
 }
+
+const whiteModalVariant = cva(
+  "border bg-white rounded-lg flex p-6 shadow-lg w-[320px] transition items-center duration-500", // 기본 스타일
+  {
+    variants: {
+      isDisplayed: {
+        true: "translate-x-0", // 나타날 때
+        false: "translate-x-[calc(100%+40px)]", // 안 보일 때
+      },
+    },
+    defaultVariants: {
+      isDisplayed: true,
+    },
+  }
+);
 function WhiteModal({ toast }: WhiteModalProps) {
   const [isDisplayed, setIsDisplayed] = useState<boolean>(false);
 
@@ -17,16 +32,7 @@ function WhiteModal({ toast }: WhiteModalProps) {
   };
 
   return (
-    <div
-      className={clsx(
-        "border bg-white rounded-lg flex p-6 shadow-lg w-[320px] transition items-center duration-500",
-        {
-          "translate-x-0": isDisplayed, // 나타날때
-          "translate-x-[calc(100%+40px)]": !isDisplayed, // 안보일때
-        }
-      )}
-      onClick={handleDelete}
-    >
+    <div className={whiteModalVariant({ isDisplayed })} onClick={handleDelete}>
       <div className="flex flex-col">
         <h5 className="font-semibold">{toast.text}</h5>
       </div>
